12774215b927062597adbdd841412341bb731abc
Rafael Choinhet <rafael.choinhet@looqbox.com>
2025-01-02 08:04:34 -0300

    feat: init menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,py}"],
+  content: ["./python_graph/**/*.{html,js,ts,py}"],
   theme: {
     extend: {
       fontFamily: {
